e8901c981e7737a06d0917f3a7d15ed7c4bc515a
Matt Hasson <scrummer03@gmail.com>
2023-09-13 18:16:49 -0500

    Changed onclick to addEventListener

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let count = 0
 let total = 0
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let totalEl = document.getElementById("total-el")
+
+incrementBtn.addEventListener("click", increment)
+saveBtn.addEventListener("click", save)
 
 function increment() {
     count += 1
